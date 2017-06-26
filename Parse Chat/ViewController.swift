df10fb95ed1e551dd75af16264df89a6eb1f9573
Gabriel Muniz <gabrielm854@fb.com>
2017-06-26 11:25:15 -0700

    Initial Commit

---
+++ Parse Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parse Chat
+//
+//  Created by Gabriel Muñiz on 6/26/17.
+//  Copyright © 2017 Gabriel Muñiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
